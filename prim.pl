e9e92ef4d60ae6f18806e52f2ea229a0283a85f0
sam6816 <52503040+sam6816@users.noreply.github.com>
2019-12-02 18:44:36 +0000

    Add files via upload

---
+++ prim.pl
@@ -0,0 +1,77 @@
+# Eratosthenes' Sieve 
+# 
+$MAX = 4;
+
+init();
+prstep();
+
+while (++$i <= $MAX) {
+
+  # new p: first two steps are direct old p plus 1 -- pattern is too short 
+  # 
+  $n = $p[$i-1] + 1;
+  $p[$i] = $n > $size ?  $n : p_next(); 
+
+  # Replicate pattern times p:
+  #
+  @{$pattern[$i]} = ( @{$pattern[$i-1]} ) x $p[$i];
+
+  $size = scalar @{$pattern[$i]}; 
+
+  # Sieve each p'th element
+  #
+  $sieved = 0;
+  for ( $j =  $p[$i]-1; $j <= $size; $j += $p[$i] ) {
+
+        if ($pattern[$i][$j] eq 'x') {
+
+            $pattern[$i][$j] =  '.'; 
+	    $sieved++;
+#				printf "S: %5d\t", $j+1;
+        }
+  }
+  # print this step
+  prstep();
+}
+exit;
+
+sub prstep {
+  # print pattern and other per-step variables 
+  #
+  print "\nsieved:$sieved (Step=$i), p=$p[$i], len=$size\n";
+  print @{$pattern[$i]}, "\n"; 
+  # or:
+  # pr_block();
+  return;
+}
+
+sub pr_block {
+  # print pattern aligned in p lines 
+  #
+  $s = $size/$p[$i];
+  print "$s\n";
+  for ($k=0; $k<$size; $k+=$s) {
+    print @{$pattern[$i]}[$k .. $k+$s-1], "\n";
+  } 
+  return;
+}
+
+sub p_next {
+  # this is the next 'x' after old p[i], for steps > 2
+  #
+  until($pattern[$i-1][$n-1] eq 'x') {
+    $n++;
+  } 
+  return $n;
+} 
+
+sub init {
+  # "$i" is index/step
+  #
+  $i      = 0; 
+  $p[$i]  = 1;
+
+  @{$pattern[$i]} = ('x'); 
+
+  $size = 1;
+}
